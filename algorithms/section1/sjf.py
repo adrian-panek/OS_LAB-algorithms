10dbe6d36f6ece6f1c028764f77e2cd8bd1c6437
adrian-panek <adrian.panek999@gmail.com>
2021-12-26 21:21:30 +0100

    initial commit, first scratch of two algorithms

---
+++ algorithms/section1/sjf.py
@@ -0,0 +1,33 @@
+from collections import namedtuple
+import csv
+
+Process = namedtuple("Process", "arrival_time burst_time")
+processes = []
+
+with open("test_data.csv") as csvfile:
+    filereader = csv.reader(csvfile, delimiter=",")
+    for row in filereader:
+        processes.append(Process(row[0], row[1]))
+
+sorted_processes = sorted(processes, key=lambda x: x.burst_time)
+
+completion_time = []
+for i in range(len(sorted_processes)):
+    if (i==0):
+        completion_time.append(int(sorted_processes[0][1]))
+    else:
+        completion_time.append(int(completion_time[i-1]) + int(sorted_processes[i][1]))
+
+turn_around_time = []
+for i in range(len(sorted_processes)):
+    turn_around_time.append(int(completion_time[i]) - int(sorted_processes[i][1]))
+
+waiting_time = []
+for i in range(len(sorted_processes)):
+    waiting_time.append(int(turn_around_time[i]) - int(sorted_processes[i][0]))
+
+for i in range(len(sorted_processes)):
+    print(f"Process: {i+1}")
+    print(f"CT: {completion_time[i]}")
+    print(f"WT: {waiting_time[i]}")
+    print("--------------")
